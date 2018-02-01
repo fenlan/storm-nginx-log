81b7b5fe2acda0b154797d0dbab820fd4ade9dad
fenlan <1475307818@qq.com>
2018-02-02 02:11:08 +0800

    main

--- src/main/java/com/fenlan/storm/storm/NginxStorm.java
+++ src/main/java/com/fenlan/storm/storm/NginxStorm.java
@@ -1,4 +1,24 @@
 package com.fenlan.storm.storm;
 
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.topology.TopologyBuilder;
+
 public class NginxStorm {
+    public static void main(String[] argv) throws InterruptedException {
+
+        Config config = new Config();
+        config.setDebug(true);
+
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("LogSpout", new LogSpout(), 2);
+        builder.setBolt("SpliteBolt", new SpliteBolt(), 4);
+
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("NginxLog", config, builder.createTopology());
+        Thread.sleep(10000);
+
+        cluster.killTopology("NginxLog");
+        cluster.shutdown();
+    }
 }
