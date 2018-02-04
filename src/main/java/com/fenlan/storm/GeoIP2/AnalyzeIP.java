cd1e2ba4fc330d33cdb3ddc355159361a26ba4bc
fenlan <1475307818@qq.com>
2018-02-05 01:25:49 +0800

    根据IP地址分析物理位置

---
+++ src/main/java/com/fenlan/storm/GeoIP2/AnalyzeIP.java
@@ -0,0 +1,33 @@
+package com.fenlan.storm.GeoIP2;
+
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CityResponse;
+import com.maxmind.geoip2.record.City;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class AnalyzeIP {
+
+    private static String dbFile = "/home/fenlan/Downloads/GeoLite2-City.mmdb";
+
+    public static String cityOfIP(String ip) throws IOException, GeoIp2Exception {
+        // A File object pointing to your GeoIP2 or GeoLite2 database
+        File database = new File(dbFile);
+
+        // This creates the DatabaseReader object. To improve performance, reuse
+        // the object across lookups. The object is thread-safe
+        DatabaseReader reader = new DatabaseReader.Builder(database).build();
+        InetAddress ipAddress = InetAddress.getByName(ip);
+
+        // Replace "city" with the appropriate method for your database, e.g.,
+        // "country".
+        CityResponse response = reader.city(ipAddress);
+
+        City city = response.getCity();
+
+        return city.getNames().get("zh-CN");
+    }
+}
