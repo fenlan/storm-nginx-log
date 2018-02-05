fb92ec712b0a7fb34edbcdc705eb9aceb662bec6
fenlan <1475307818@qq.com>
2018-02-05 22:53:20 +0800

    配置Redis数据库

--- src/main/java/com/fenlan/storm/Properties/RedisProperties.java
+++ src/main/java/com/fenlan/storm/Properties/RedisProperties.java
@@ -1,4 +1,19 @@
 package com.fenlan.storm.Properties;
 
+import java.util.ResourceBundle;
+
 public class RedisProperties {
+
+    public static String getRedisHost() {
+
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("application");
+
+        return resourceBundle.getString("redis.host");
+    }
+    public static int getredisPort() {
+
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("application");
+
+        return Integer.parseInt(resourceBundle.getString("redis.port"));
+    }
 }
