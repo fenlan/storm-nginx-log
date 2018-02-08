e69bcf4e2886fecbfbb225ec378747c999f12005
fenlan <1475307818@qq.com>
2018-02-09 00:16:37 +0800

    test

--- src/test/java/ReadLine.java
+++ src/test/java/ReadLine.java
@@ -6,6 +6,9 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -47,5 +50,13 @@ public class ReadLine {
         } catch (GeoIp2Exception e) {
             System.out.println("ip 没有找到");
         }
+
+        String time_local = matcher.group(4).substring(1, matcher.group(4).length()-1);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss Z", Locale.ENGLISH);
+        LocalDateTime dateTime = LocalDateTime.parse(time_local, formatter);
+        System.out.println("date : " + dateTime + " " + dateTime.getMonthValue() + " " + dateTime.getDayOfMonth());
+        System.out.println(20180208 / 10000);
+        System.out.println(20180208 % 100);
+        System.out.println((20180208 - 20180208 / 10000 * 10000) / 100);
     }
 }
