1caf985ed1696fc0cf88db3946401f316d45efb2
fenlan <1475307818@qq.com>
2018-03-15 20:19:40 +0800

    Kafka消费者&Spout

--- src/main/java/com/fenlan/storm/storm/LogSpout.java
+++ src/main/java/com/fenlan/storm/storm/LogSpout.java
@@ -1,63 +1,49 @@
 package com.fenlan.storm.storm;
 
-import com.fenlan.storm.Properties.FileProperties;
+import com.fenlan.storm.Properties.KafkaProperties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.storm.spout.SpoutOutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichSpout;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Values;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Map;
-import java.util.stream.Stream;
+import java.util.Properties;
 
 public class LogSpout extends BaseRichSpout {
 
     private SpoutOutputCollector collector;
     private TopologyContext context;
-    private static String srcFile = FileProperties.getLogPath();
-    private static long index = 0;
+    private static String kafkaClusterIP = KafkaProperties.getKafkaCluster();
+    private static String kafkaTopic = KafkaProperties.getKafkaTopic();
+    private static Properties props = new Properties();
+    private static KafkaConsumer<String, String> consumer;
 
     @Override
     public void open(Map map, TopologyContext topologyContext, SpoutOutputCollector spoutOutputCollector) {
         this.collector = spoutOutputCollector;
         this.context = topologyContext;
+
+        props.put("bootstrap.servers", kafkaClusterIP);//kafka clusterIP
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.offset.reset", "earliest");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList(kafkaTopic));
     }
 
     @Override
     public void nextTuple() {
-        long lines = 0;
-        String record = null;
-
-        try {
-            Stream<String> fileStream = Files.lines(Paths.get(srcFile));
-            lines = fileStream.count();
-            fileStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (lines != index) {
-            if (lines < index)      index = 0;
-            for (long i = index; i < lines; i++) {
-                try {
-                    List<String> fileStream = Files.readAllLines(Paths.get(srcFile));
-                    record = fileStream.get((int)i);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                this.collector.emit(new Values(i, record));
-            }
-            index = lines;
-        }
-        else {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        ConsumerRecords<String, String> records = consumer.poll(100);
+        for (ConsumerRecord<String, String> record : records){
+            System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+            this.collector.emit(new Values(record.key(), record.value()));
         }
     }
 
