38d39db29c905a255d61039bb0b5a45711fe4d16
fenlan <1475307818@qq.com>
2018-03-15 20:17:02 +0800

    Kafka配置

---
+++ src/main/java/com/fenlan/storm/Properties/KafkaProperties.java
@@ -0,0 +1,20 @@
+package com.fenlan.storm.Properties;
+
+import java.util.ResourceBundle;
+
+public class KafkaProperties {
+
+    public static String getKafkaCluster() {
+
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("application");
+
+        return resourceBundle.getString("kafkaCluster.hosts");
+    }
+
+    public static String getKafkaTopic() {
+
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("application");
+
+        return resourceBundle.getString("kafkaTopic");
+    }
+}
---
+++ src/main/java/com/fenlan/storm/logSource/GetLogRecord.java
@@ -0,0 +1,4 @@
+package com.fenlan.storm.logSource;
+
+public class GetLogRecord {
+}
